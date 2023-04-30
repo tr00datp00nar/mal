3884a3abc233c49791b9b99e3e5de4122f0c2ae2
Micah <tr00datp00nar@gmail.com>
2023-04-30 15:40:04 -0400

    Move to nagato api wrapper

--- mal.go
+++ mal.go
@@ -1,171 +1,230 @@
 package mal
 
 import (
-	"context"
-	"flag"
-
-	"github.com/charmbracelet/log"
-	"github.com/nstratos/go-myanimelist/mal"
+	"github.com/rl404/nagato"
 )
 
-func runWatching() error {
-	config, err := LoadConfigFile()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var (
-		clientID     = flag.String("client-id", config.ClientId, "your registered MyAnimeList.net application client ID")
-		clientSecret = flag.String("client-secret", config.ClientSecret, "your registered MyAnimeList.net application client secret; optional if you chose App Type 'other'")
-		state        = flag.String("state", "", "token to protect against CSRF attacks")
+func GetUserAnimeListWatching() (response []nagato.UserAnime) {
+	d, _, err := nagatoClient.GetUserAnimeList(nagato.GetUserAnimeListParam{
+		Username: "@me",
+		Status:   nagato.UserAnimeStatusWatching,
+		NSFW:     true,
+		Sort:     nagato.UserAnimeSortUpdatedAt,
+		Offset:   0,
+		// Limit:    -1,
+	},
+		nagato.AnimeFieldAlternativeTitles,
+		nagato.AnimeFieldStartDate,
+		nagato.AnimeFieldEndDate,
+		nagato.AnimeFieldSynopsis,
+		nagato.AnimeFieldMean,
+		nagato.AnimeFieldRank,
+		nagato.AnimeFieldPopularity,
+		nagato.AnimeFieldNumListUsers,
+		nagato.AnimeFieldNumScoringUsers,
+		nagato.AnimeFieldNSFW,
+		nagato.AnimeFieldGenres,
+		nagato.AnimeFieldMediaType,
+		nagato.AnimeFieldStatus,
+		nagato.AnimeFieldMyListStatus,
+		nagato.AnimeFieldNumEpisodes,
+		nagato.AnimeFieldStartSeason,
+		nagato.AnimeFieldBroadcast,
+		nagato.AnimeFieldSource,
+		nagato.AnimeFieldAverageEpisodeDuration,
+		nagato.AnimeFieldRating,
+		nagato.AnimeFieldStudios,
+		nagato.AnimeFieldNumFavorites,
+		nagato.AnimeFieldUserStatus(nagato.UserAnimeTags),
+		nagato.AnimeFieldUserStatus(nagato.UserAnimeComments),
 	)
-	flag.Parse()
-
-	ctx := context.Background()
-
-	tokenClient, err := authenticate(ctx, *clientID, *clientSecret, *state)
 	if err != nil {
-		return err
+		panic(err)
 	}
-
-	c := client{
-		Client: mal.NewClient(tokenClient),
-	}
-
-	return c.showcaseWatching(ctx)
+	return d
 }
 
-func runPlanToWatch() error {
-	config, err := LoadConfigFile()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var (
-		clientID     = flag.String("client-id", config.ClientId, "your registered MyAnimeList.net application client ID")
-		clientSecret = flag.String("client-secret", config.ClientSecret, "your registered MyAnimeList.net application client secret; optional if you chose App Type 'other'")
-		state        = flag.String("state", "", "token to protect against CSRF attacks")
+func GetUserAnimeListPlanToWatch() (response []nagato.UserAnime) {
+	d, _, err := nagatoClient.GetUserAnimeList(nagato.GetUserAnimeListParam{
+		Username: "@me",
+		Status:   nagato.UserAnimeStatusPlanToWatch,
+		NSFW:     true,
+		Sort:     nagato.UserAnimeSortUpdatedAt,
+		Offset:   0,
+		// Limit:    -1,
+	},
+		nagato.AnimeFieldAlternativeTitles,
+		nagato.AnimeFieldStartDate,
+		nagato.AnimeFieldEndDate,
+		nagato.AnimeFieldSynopsis,
+		nagato.AnimeFieldMean,
+		nagato.AnimeFieldRank,
+		nagato.AnimeFieldPopularity,
+		nagato.AnimeFieldNumListUsers,
+		nagato.AnimeFieldNumScoringUsers,
+		nagato.AnimeFieldNSFW,
+		nagato.AnimeFieldGenres,
+		nagato.AnimeFieldMediaType,
+		nagato.AnimeFieldStatus,
+		nagato.AnimeFieldMyListStatus,
+		nagato.AnimeFieldNumEpisodes,
+		nagato.AnimeFieldStartSeason,
+		nagato.AnimeFieldBroadcast,
+		nagato.AnimeFieldSource,
+		nagato.AnimeFieldAverageEpisodeDuration,
+		nagato.AnimeFieldRating,
+		nagato.AnimeFieldStudios,
+		nagato.AnimeFieldNumFavorites,
+		nagato.AnimeFieldUserStatus(nagato.UserAnimeTags),
+		nagato.AnimeFieldUserStatus(nagato.UserAnimeComments),
 	)
-	flag.Parse()
-
-	ctx := context.Background()
-
-	tokenClient, err := authenticate(ctx, *clientID, *clientSecret, *state)
 	if err != nil {
-		return err
+		panic(err)
 	}
-
-	c := client{
-		Client: mal.NewClient(tokenClient),
-	}
-
-	return c.showcasePlanToWatch(ctx)
+	return d
 }
 
-func runCompleted() error {
-	config, err := LoadConfigFile()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var (
-		clientID     = flag.String("client-id", config.ClientId, "your registered MyAnimeList.net application client ID")
-		clientSecret = flag.String("client-secret", config.ClientSecret, "your registered MyAnimeList.net application client secret; optional if you chose App Type 'other'")
-		state        = flag.String("state", "", "token to protect against CSRF attacks")
+func GetUserAnimeListCompleted() (response []nagato.UserAnime) {
+	d, _, err := nagatoClient.GetUserAnimeList(nagato.GetUserAnimeListParam{
+		Username: "@me",
+		Status:   nagato.UserAnimeStatusCompleted,
+		NSFW:     true,
+		Sort:     nagato.UserAnimeSortUpdatedAt,
+		Offset:   0,
+		// Limit:    -1,
+	},
+		nagato.AnimeFieldAlternativeTitles,
+		nagato.AnimeFieldStartDate,
+		nagato.AnimeFieldEndDate,
+		nagato.AnimeFieldSynopsis,
+		nagato.AnimeFieldMean,
+		nagato.AnimeFieldRank,
+		nagato.AnimeFieldPopularity,
+		nagato.AnimeFieldNumListUsers,
+		nagato.AnimeFieldNumScoringUsers,
+		nagato.AnimeFieldNSFW,
+		nagato.AnimeFieldGenres,
+		nagato.AnimeFieldMediaType,
+		nagato.AnimeFieldStatus,
+		nagato.AnimeFieldMyListStatus,
+		nagato.AnimeFieldNumEpisodes,
+		nagato.AnimeFieldStartSeason,
+		nagato.AnimeFieldBroadcast,
+		nagato.AnimeFieldSource,
+		nagato.AnimeFieldAverageEpisodeDuration,
+		nagato.AnimeFieldRating,
+		nagato.AnimeFieldStudios,
+		nagato.AnimeFieldNumFavorites,
+		nagato.AnimeFieldUserStatus(nagato.UserAnimeTags),
+		nagato.AnimeFieldUserStatus(nagato.UserAnimeComments),
 	)
-	flag.Parse()
-
-	ctx := context.Background()
-
-	tokenClient, err := authenticate(ctx, *clientID, *clientSecret, *state)
 	if err != nil {
-		return err
+		panic(err)
 	}
-
-	c := client{
-		Client: mal.NewClient(tokenClient),
-	}
-
-	return c.showcaseCompleted(ctx)
+	return d
 }
 
-func runMangaList() error {
-	config, err := LoadConfigFile()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var (
-		clientID     = flag.String("client-id", config.ClientId, "your registered MyAnimeList.net application client ID")
-		clientSecret = flag.String("client-secret", config.ClientSecret, "your registered MyAnimeList.net application client secret; optional if you chose App Type 'other'")
-		state        = flag.String("state", "", "token to protect against CSRF attacks")
+func GetUserMangaList() (response []nagato.UserManga) {
+	d, _, err := nagatoClient.GetUserMangaList(nagato.GetUserMangaListParam{
+		Username: "@me",
+		Status:   nagato.UserMangaStatusReading,
+		NSFW:     true,
+		Sort:     nagato.UserMangaSortUpdatedAt,
+		Offset:   0,
+		// Limit:    -1,
+	},
+		nagato.MangaFieldAlternativeTitles,
+		nagato.MangaFieldStartDate,
+		nagato.MangaFieldEndDate,
+		nagato.MangaFieldSynopsis,
+		nagato.MangaFieldMean,
+		nagato.MangaFieldRank,
+		nagato.MangaFieldPopularity,
+		nagato.MangaFieldNumListUsers,
+		nagato.MangaFieldNumScoringUsers,
+		nagato.MangaFieldNSFW,
+		nagato.MangaFieldGenres,
+		nagato.MangaFieldMediaType,
+		nagato.MangaFieldStatus,
+		nagato.MangaFieldMyListStatus,
+		nagato.MangaFieldNumVolumes,
+		nagato.MangaFieldNumChapters,
+		nagato.MangaFieldAuthors,
+		nagato.MangaFieldNumFavorites,
+		nagato.MangaFieldUserStatus(nagato.UserMangaTags),
 	)
-	flag.Parse()
-
-	ctx := context.Background()
-
-	tokenClient, err := authenticate(ctx, *clientID, *clientSecret, *state)
 	if err != nil {
-		return err
+		panic(err)
 	}
-
-	c := client{
-		Client: mal.NewClient(tokenClient),
-	}
-
-	return c.showcaseMangaList(ctx)
+	return d
 }
 
-func runMangaListPlan() error {
-	config, err := LoadConfigFile()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var (
-		clientID     = flag.String("client-id", config.ClientId, "your registered MyAnimeList.net application client ID")
-		clientSecret = flag.String("client-secret", config.ClientSecret, "your registered MyAnimeList.net application client secret; optional if you chose App Type 'other'")
-		state        = flag.String("state", "", "token to protect against CSRF attacks")
+func GetUserMangaListPlanToRead() (response []nagato.UserManga) {
+	d, _, err := nagatoClient.GetUserMangaList(nagato.GetUserMangaListParam{
+		Username: "@me",
+		Status:   nagato.UserMangaStatusPlanToRead,
+		NSFW:     true,
+		Sort:     nagato.UserMangaSortUpdatedAt,
+		Offset:   0,
+		// Limit:    -1,
+	},
+		nagato.MangaFieldAlternativeTitles,
+		nagato.MangaFieldStartDate,
+		nagato.MangaFieldEndDate,
+		nagato.MangaFieldSynopsis,
+		nagato.MangaFieldMean,
+		nagato.MangaFieldRank,
+		nagato.MangaFieldPopularity,
+		nagato.MangaFieldNumListUsers,
+		nagato.MangaFieldNumScoringUsers,
+		nagato.MangaFieldNSFW,
+		nagato.MangaFieldGenres,
+		nagato.MangaFieldMediaType,
+		nagato.MangaFieldStatus,
+		nagato.MangaFieldMyListStatus,
+		nagato.MangaFieldNumVolumes,
+		nagato.MangaFieldNumChapters,
+		nagato.MangaFieldAuthors,
+		nagato.MangaFieldNumFavorites,
+		nagato.MangaFieldUserStatus(nagato.UserMangaTags),
 	)
-	flag.Parse()
-
-	ctx := context.Background()
-
-	tokenClient, err := authenticate(ctx, *clientID, *clientSecret, *state)
 	if err != nil {
-		return err
+		panic(err)
 	}
-
-	c := client{
-		Client: mal.NewClient(tokenClient),
-	}
-
-	return c.showcaseMangaListPlan(ctx)
+	return d
 }
 
-func runMangaListDone() error {
-	config, err := LoadConfigFile()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var (
-		clientID     = flag.String("client-id", config.ClientId, "your registered MyAnimeList.net application client ID")
-		clientSecret = flag.String("client-secret", config.ClientSecret, "your registered MyAnimeList.net application client secret; optional if you chose App Type 'other'")
-		state        = flag.String("state", "", "token to protect against CSRF attacks")
+func GetUserMangaListDone() (response []nagato.UserManga) {
+	d, _, err := nagatoClient.GetUserMangaList(nagato.GetUserMangaListParam{
+		Username: "@me",
+		Status:   nagato.UserMangaStatusCompleted,
+		NSFW:     true,
+		Sort:     nagato.UserMangaSortUpdatedAt,
+		Offset:   0,
+		// Limit:    -1,
+	},
+		nagato.MangaFieldAlternativeTitles,
+		nagato.MangaFieldStartDate,
+		nagato.MangaFieldEndDate,
+		nagato.MangaFieldSynopsis,
+		nagato.MangaFieldMean,
+		nagato.MangaFieldRank,
+		nagato.MangaFieldPopularity,
+		nagato.MangaFieldNumListUsers,
+		nagato.MangaFieldNumScoringUsers,
+		nagato.MangaFieldNSFW,
+		nagato.MangaFieldGenres,
+		nagato.MangaFieldMediaType,
+		nagato.MangaFieldStatus,
+		nagato.MangaFieldMyListStatus,
+		nagato.MangaFieldNumVolumes,
+		nagato.MangaFieldNumChapters,
+		nagato.MangaFieldAuthors,
+		nagato.MangaFieldNumFavorites,
+		nagato.MangaFieldUserStatus(nagato.UserMangaTags),
 	)
-	flag.Parse()
-
-	ctx := context.Background()
-
-	tokenClient, err := authenticate(ctx, *clientID, *clientSecret, *state)
 	if err != nil {
-		return err
+		panic(err)
 	}
-
-	c := client{
-		Client: mal.NewClient(tokenClient),
-	}
-
-	return c.showcaseMangaListDone(ctx)
+	return d
 }
--- server.go
+++ server.go
@@ -1,40 +1,39 @@
 package mal
 
 import (
-	"bufio"
 	"context"
-	"crypto/rand"
 	"encoding/json"
-	"fmt"
-	"net/http"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"runtime"
 
 	"github.com/charmbracelet/log"
+	"github.com/rl404/nagato"
+	"github.com/rl404/nagato/mal"
 	"github.com/spf13/viper"
-	"golang.org/x/oauth2"
 )
 
-// TODO: Move from using viper for configuration to the rwxrob/conf package instead.
-//		c := conf.C(Id: `bar`, Dir: `testdata`, File: `config.yaml`)
-//		clientId := c.Query(.mal.client_id)
-//		clientId := c.Query(.mal.client_secret)
+var (
+	userHomeDir, _ = os.UserHomeDir()
+	cacheDir       = filepath.Join(userHomeDir, ".config/mal")
+	cacheName      = filepath.Join(cacheDir, "/mal-token-cache.txt")
 
-var userHomeDir, _ = os.UserHomeDir()
-var cacheDir = filepath.Join(userHomeDir, ".config/mal")
-var cacheName = filepath.Join(cacheDir, "/mal-token-cache.txt")
+	nagatoClient *nagato.Client
+	config       MalConfig
+
+	clientID     = config.ClientId
+	clientSecret = config.ClientSecret
+	state        = ""
+)
 
 type MalConfig struct {
-	ClientId     string `yaml:"mal_client_id"`
-	ClientSecret string `yaml:"mal_client_secret"`
+	ClientId     string `yaml:"mal.client_id"`
+	ClientSecret string `yaml:"mal.client_secret"`
 }
 
 func LoadConfigFile() (config MalConfig, err error) {
-	viper.AddConfigPath(filepath.Clean("$HOME/.config/mal"))
-	viper.SetConfigName("mal")
+	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
+	viper.AddConfigPath(filepath.Clean("$HOME/.config/c"))
 
 	err = viper.ReadInConfig()
 	if err != nil {
@@ -44,127 +43,44 @@ func LoadConfigFile() (config MalConfig, err error) {
 	return
 }
 
-func authenticate(ctx context.Context, clientID, clientSecret, state string) (*http.Client, error) {
-	// Prepare the oauth2 configuration with your application ID, secret, the
-	// MyAnimeList authentication and token URLs as specified in:
-	//
-	// https://myanimelist.net/apiconfig/references/authorization
-	conf := &oauth2.Config{
+func clientPublic() {
+	nagatoClient = nagato.New(clientID)
+}
+
+func clientOauth2() {
+	nagatoClient = nagato.New(clientID)
+
+	// Generating token should be done outside your project flow
+	// and should be done one time only.
+	if err := generateToken(context.Background(), clientID, clientSecret, state); err != nil {
+		panic(err)
+	}
+
+	cachedToken, err := loadCachedToken()
+	if err != nil {
+		panic(err)
+	}
+
+	malWithOauth2, err := mal.NewWithOauth2(mal.Oauth2Config{
 		ClientID:     clientID,
 		ClientSecret: clientSecret,
-		Endpoint: oauth2.Endpoint{
-			AuthURL:   "https://myanimelist.net/v1/oauth2/authorize",
-			TokenURL:  "https://myanimelist.net/v1/oauth2/token",
-			AuthStyle: oauth2.AuthStyleInParams,
-		},
-	}
-
-	oauth2Token, err := loadCachedToken()
-	if err == nil {
-		refreshedToken, err := conf.TokenSource(ctx, oauth2Token).Token()
-		if err == nil && (oauth2Token != refreshedToken) {
-			fmt.Println("Caching refreshed oauth2 token...")
-			if err := cacheToken(*refreshedToken); err != nil {
-				return nil, fmt.Errorf("caching refreshed oauth2 token: %s", err)
-			}
-			return conf.Client(ctx, refreshedToken), nil
-		}
-		return conf.Client(ctx, oauth2Token), nil
-	}
-
-	// Generate a code verifier, a high-entropy cryptographic random string. It
-	// will be set as the code_challenge in the authentication URL. It should
-	// have a minimum length of 43 characters and a maximum length of 128
-	// characters.
-	const codeVerifierLength = 128
-	codeVerifier, err := generateCodeVerifier(codeVerifierLength)
+		State:        state,
+		AccessToken:  cachedToken.AccessToken,
+		RefreshToken: cachedToken.RefreshToken,
+		Expiry:       cachedToken.Expiry,
+	})
 	if err != nil {
-		return nil, fmt.Errorf("generating code verifier: %v", err)
+		panic(err)
 	}
 
-	// Produce the authentication URL where the user needs to be redirected and
-	// allow your application to access their MyAnimeList data.
-	authURL := conf.AuthCodeURL(state,
-		oauth2.SetAuthURLParam("code_challenge", codeVerifier),
-	)
-	err = openBrowser(authURL)
-	if err != nil {
-		fmt.Println("Could not open browser.")
-	}
-
-	fmt.Printf("Your browser should open: %v\n", authURL)
-	fmt.Print("After authenticating, copy the code from the browser URL and paste it here: ")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	code := scanner.Text()
-	if scanner.Err() != nil {
-		return nil, fmt.Errorf("reading code from terminal: %v", err)
-	}
-
-	// Exchange the authentication code for a token. MyAnimeList currently only
-	// supports the plain code_challenge_method so to verify the string, just
-	// make sure it is the same as the one you entered in the code_challenge.
-	token, err := conf.Exchange(ctx, code,
-		oauth2.SetAuthURLParam("code_verifier", codeVerifier),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("exchanging code for token: %v", err)
-	}
-	fmt.Println("Authentication was successful. Caching oauth2 token...")
-	if err := cacheToken(*token); err != nil {
-		return nil, fmt.Errorf("caching oauth2 token: %s", err)
-	}
-
-	return conf.Client(ctx, token), nil
+	nagatoClient.SetMalClient(malWithOauth2)
 }
 
-func cacheToken(token oauth2.Token) error {
-	b, err := json.MarshalIndent(token, "", "   ")
-	if err != nil {
-		return fmt.Errorf("marshaling token %s: %v", token, err)
-	}
-	err = os.WriteFile(cacheName, b, 0644)
-	if err != nil {
-		return fmt.Errorf("writing token %s to file %q: %v", token, cacheName, err)
-	}
-	return nil
-}
+func toJson(name string, d interface{}) {
+	jsonData, _ := json.MarshalIndent(d, "", " ")
 
-func loadCachedToken() (*oauth2.Token, error) {
-	b, err := os.ReadFile(cacheName)
-	if err != nil {
-		return nil, fmt.Errorf("reading oauth2 token from cache file %q: %v", cacheName, err)
-	}
-	token := new(oauth2.Token)
-	if err := json.Unmarshal(b, token); err != nil {
-		return nil, fmt.Errorf("unmarshaling oauth2 token: %v", err)
-	}
-	return token, nil
-}
+	f, _ := os.Create(name + ".json")
+	defer f.Close()
 
-func generateCodeVerifier(length int) (string, error) {
-	const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-		"abcdefghijklmnopqrstvuwxyz" +
-		"0123456789-._~"
-	bytes := make([]byte, length)
-	if _, err := rand.Read(bytes); err != nil {
-		return "", err
-	}
-	for i, b := range bytes {
-		bytes[i] = charset[b%byte(len(charset))]
-	}
-	return string(bytes), nil
-}
-
-func openBrowser(url string) error {
-	switch runtime.GOOS {
-	case "linux":
-		return exec.Command("xdg-open", url).Start()
-	case "windows":
-		return exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	case "darwin":
-		return exec.Command("open", url).Start()
-	default:
-		return fmt.Errorf("openBrowser: unsupported operating system: %v", runtime.GOOS)
-	}
+	f.Write(jsonData)
 }
